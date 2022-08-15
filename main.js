ce4aae9771d688cffdd5638629b4177a71b4354f
Mrinal Sarkar <106019584+VickySarkar690@users.noreply.github.com>
2022-08-16 01:22:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const article = document.getElementById("article");
+
+async function fetchPosts()
+{
+  const response = await fetch(
+    "https://fakestoreapi.com/products"
+  );
+  const data = await response.json();
+  return data;
+}
+
+fetchPosts().then(data => {
+    data.map((post) => {
+        const article = document.createElement("article");
+        article.innerHTML = `
+          <h2>${post.title}</h2>
+          <img src=${post.image}></img>
+          <div>
+            <h3>${post.price}</h3>
+          <p>${post.description}</p>
+          </div>
+        `;
+        document.body.appendChild(article);
+      });
+});
+
